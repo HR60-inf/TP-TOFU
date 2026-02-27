0674ec3a4ade4bce9688f169a57e7bc22b84796a
Ismael <rahalismael37@gmail.com>
2026-02-27 10:28:45 +0100

    feat: section 4 - backend S3 + GitHub Actions workflow

--- tofu/main.tf
+++ tofu/main.tf
@@ -6,6 +6,16 @@ terraform {
       version = "~> 1.53"
     }
   }
+  backend "s3" {
+    bucket                      = "tp-iac-hr60"
+    key                         = "terraform.tfstate"
+    region                      = "gra"
+    endpoint                    = "https://s3.gra.perf.cloud.ovh.net"
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+  }
 }
 
 provider "openstack" {
@@ -17,11 +27,12 @@ resource "openstack_compute_instance_v2" "server" {
   image_name  = "Ubuntu 22.04"
   flavor_name = "d2-2"
   key_pair    = "ma-cle-wsl"
-  
-   network {
+
+  network {
     name = "Ext-Net"
     uuid = "b2c02fdc-ffdf-40f6-9722-533bd7058c06"
-  }  
+  }
+
   security_groups = ["default"]
 }
 
